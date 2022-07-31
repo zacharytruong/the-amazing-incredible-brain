4e35ddc439c249c2d19fdd59ebc7085fb652e1f9
Zachary Truong <zacharytruong@gmail.com>
2022-07-31 09:20:42 -0500

    add Scroll to top

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import Home from './components/Home';
 import Author from './components/Author';
 import Fact from './components/Fact';
 import Fiction from './components/Fiction';
+import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop>
         <Routes>
           <Route path="/the-amazing-incredible-brain/" element={<Home />} />
-          <Route path="/the-amazing-incredible-brain/author" element={<Author />} />
+          <Route
+            path="/the-amazing-incredible-brain/author"
+            element={<Author />}
+          />
           <Route path="/the-amazing-incredible-brain/fact" element={<Fact />} />
-          <Route path="/the-amazing-incredible-brain/fiction" element={<Fiction />} />
+          <Route
+            path="/the-amazing-incredible-brain/fiction"
+            element={<Fiction />}
+          />
         </Routes>
-      </BrowserRouter>
+      </ScrollToTop>
+    </BrowserRouter>
   );
 }
 
